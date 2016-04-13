4ad8c4f9ae9d0e9d7094108f43c114963a8e9642
EC2 Default User <ec2-user@ip-10-0-0-195.us-west-2.compute.internal>
2016-04-13 22:35:08 +0000

    fix javascript error

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,14 +1,14 @@
 class RankingsController < ApplicationController 
     def index
-        if !(params[:sdate]) then params[:sdate] = "2016-02-01" end
-        if !(params[:edate]) then params[:edate] = "2016-02-01" end
-        if !(params[:ranking_type]) then params[:ranking_type] = "raw" end
+        if (params[:sdate].blank?) then params[:sdate] = "2016-02-01" end
+        if (params[:edate].blank?) then params[:edate] = "2016-02-01" end
+        if (params[:ranking_type].blank?) then params[:ranking_type] = "raw" end
 
         if params[:ranking_type].to_s == "average"
-        ranking = Ranking.select("search_date, avg(google) as google, avg(yahoo) as yahoo, avg(bing) as bing").where("search_date >= ? AND search_date <= ?", params[:sdate], params[:sdate]).group("search_date");
+        ranking = Ranking.select("search_date, avg(google) as google, avg(yahoo) as yahoo, avg(bing) as bing").where("search_date >= ? AND search_date <= ?", params[:sdate], params[:edate]).group("search_date");
         @rankings = ranking.paginate(:page => params[:page], :per_page => 30)
         elsif params[:ranking_type].to_s == "weighted"
-        ranking = Ranking.select("search_date, avg(global_monthly_searches) avg_global, max(global_monthly_searches) as max_global, avg(google) as google, avg(yahoo) as yahoo, avg(bing) as bing").where("search_date >= ? AND search_date <= ?", params[:sdate], params[:sdate]).group("search_date");
+        ranking = Ranking.select("search_date, avg(global_monthly_searches) avg_global, max(global_monthly_searches) as max_global, avg(google) as google, avg(yahoo) as yahoo, avg(bing) as bing").where("search_date >= ? AND search_date <= ?", params[:sdate], params[:edate]).group("search_date");
         @rankings = ranking.paginate(:page => params[:page], :per_page => 30)
         else
         ranking = Ranking.where("search_date >= ? AND search_date <= ?", params[:sdate], params[:sdate])
